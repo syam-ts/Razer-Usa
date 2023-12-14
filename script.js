73694867c88b7229fb5caa4a801692105683d1f9
syam. <syamnandhu3@gmail.com.>
2023-12-14 20:03:34 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 
-// myFun = ()=>{
-//     document.getElementById('store').style.backgroundColor = 'blue';
-// }
+  $(document).ready(function(){
+//  For Navbar
+    $('.nav-link').mouseenter(function(){
+     
+      $(this).css({'color':'white'});
 
-// document.getElementById('store').addEventListener("drag",myFun);
+    });
+    $('.nav-link').mouseleave(function(){
+     
+      $(this).css({'color':'gray'});
+
+    });
+// For Footer
+    $('.list-group-item').mouseenter(function(){
+     
+      $(this).css({'color':'white'});
+    });
+    $('.list-group-item').mouseleave(function(){
+     
+      $(this).css({'color':'gray'});
+
+    });
+// Footer -2
+    $('footer a').mouseenter(function(){
+     
+      $(this).css({'color':'white'});
+      $(this).css({'text-decoration':'underline'});
+    });
+    $('footer a').mouseleave(function(){
+     
+      $(this).css({'color':'gray'});
+      
+    });
+
+  });
